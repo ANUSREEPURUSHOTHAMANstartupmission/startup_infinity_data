dabb875fc2c3ee3b50fa8ba115c550da970db09b
ANUSREEPURUSHOTHAMANstartupmission <121005558+ANUSREEPURUSHOTHAMANstartupmission@users.noreply.github.com>
2023-07-14 11:31:53 +0530

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ANUSREEPURUSHOTHAMANstartupmission.github.io',
+  base: '/startup_infinity_data',
   integrations: [tailwind(), svelte()]
 });
